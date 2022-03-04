6f7a29fc3664a7a83d706adafe08141035b5dcbf
Jenny Ischakov R <jenny.ischakov@gmail.com>
2022-03-04 12:41:31 -0600

    Add files via upload

--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -16,20 +16,20 @@ from selenium.webdriver.chrome.options import Options
 # ================================================= Variables
 
 # AWS driver
-# options = Options()
-# options.add_argument("--headless")
-# options.add_argument("window-size=1400,1500")
-# options.add_argument("--disable-gpu")
-# options.add_argument("--no-sandbox")
-# options.add_argument("start-maximized")
-# options.add_argument("enable-automation")
-# options.add_argument("--disable-infobars")
-# options.add_argument("--disable-dev-shm-usage")
-#
-# driver = webdriver.Chrome(options=options)
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
 
 # Local driver
-driver = webdriver.Chrome(executable_path='C:\chromedriver.exe')
+# driver = webdriver.Chrome(executable_path='C:\chromedriver.exe')
 
 action = ActionChains(driver)
 
